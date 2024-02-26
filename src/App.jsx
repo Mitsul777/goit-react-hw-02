c76d14ec28e3318b3a040a8ec4347b1e0e7f0627
MitsulArkadiy <bmwpilot777@gmail.com>
2024-02-26 15:37:51 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,54 +6,51 @@ import Notification from "./components/Notification/Notification.jsx";
 
 
 const App = () => {
-    const [clicks, setClicks] = useState({
+    const savedClicks = JSON.parse(window.localStorage.getItem("saved-clicks"));
+    const [clicks, setClicks] = useState(savedClicks || {
         good: 0,
         neutral: 0,
         bad: 0
     });
-    const [totalFeedback, settotalFeedback] = useState(false);
+    const [totalClicks, setTotalClicks] = useState(0);
     const [showReset, setShowReset] = useState(false);
-    const [totalClicks, setTotalClicks ] = useState(false);
-    const [showTotalClicks, setShowTotalClicks] = useState(false);
     const [positivePercentage, setPositivePercentage] = useState(0);
     const updateFeedback = feedbackType => {
-        setClicks(prevState => ({
-            ...prevState,
-            [feedbackType]: prevState[feedbackType] + 1
+        setClicks(prevClicks => ({
+            ...prevClicks,
+            [feedbackType]: prevClicks[feedbackType] + 1
         }));
-        setTotalClicks(prevTotalclicks => prevTotalclicks + 1)
-        settotalFeedback(true)
+        setTotalClicks(prevTotalClicks => prevTotalClicks + 1);
         setShowReset(true);
-        setShowTotalClicks(true);
     }
 
     const resetFeedback = () => {
         setClicks({
             good: 0,
             neutral: 0,
-            bad: 0,
-        })
-        setTotalClicks(0)
-        settotalFeedback(false)
+            bad: 0
+        });
+        setTotalClicks(0);
         setShowReset(false);
-        setShowTotalClicks(false);
         setPositivePercentage(0);
-
-
     };
+
     useEffect(() => {
-        if (totalFeedback > 0) {
+        if (totalClicks > 0) {
             const positiveCount = clicks.good + clicks.neutral;
-            const percentage = Math.round((positiveCount / totalFeedback) * 100);
+            const percentage = Math.round((positiveCount / totalClicks) * 100);
             setPositivePercentage(percentage);
         }
-    }, [clicks, totalFeedback]);
+    }, [clicks, totalClicks]);
+    useEffect(() => {
+        window.localStorage.setItem("saved-clicks", JSON.stringify(clicks));
+    }, [clicks]);
 
     return (
         <>
             <Description />
-            <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} showReset={showReset} showTotalClicks={showTotalClicks}  />
-            {totalFeedback > 0 ? <Feedback clicks={clicks} totalClicks={totalClicks} positivePercentage={positivePercentage} /> : <Notification />}
+            <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} showReset={showReset} />
+            {totalClicks > 0 ? <Feedback clicks={clicks} totalClicks={totalClicks} positivePercentage={positivePercentage} /> : <Notification />}
         </>
     );
 };
